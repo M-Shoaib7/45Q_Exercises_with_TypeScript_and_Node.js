1986af30d67fefe1218d2a1459ac71d95465a476
M-Shoaib7 <mshoaib249250@gmail.com>
2024-05-31 22:44:23 +0500

    Add files via upload

---
+++ Greetings/12.Greetings.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ['Shoaib', 'Sabir', 'Usama', 'Asad'];
+// is main hum erry ko use kr rhy hain [] is ko erry kehty hain.
+const message = 'Welcome to the party: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
