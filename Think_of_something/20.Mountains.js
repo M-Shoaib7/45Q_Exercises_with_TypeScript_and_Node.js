d507f51fd2ee4e7a604bab22afb70305ed722643
M-Shoaib7 <mshoaib249250@gmail.com>
2024-05-31 22:56:30 +0500

    Add files via upload

---
+++ Think_of_something/20.Mountains.js
@@ -0,0 +1,5 @@
+"use strict";
+let mountains_list = ["Mounteverest", "K2", "Nagaparbt", "Lhotse", "Makalu"];
+for (let i = 0; i < mountains_list.length; i++) {
+    console.log(mountains_list[i]);
+}
