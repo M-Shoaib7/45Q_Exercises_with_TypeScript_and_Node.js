60a1fca6f7f560271647308f96e47b94752d61c3
M-Shoaib7 <mshoaib249250@gmail.com>
2024-05-31 22:41:31 +0500

    Add files via upload

---
+++ Adding_Comments/Program_2.js
@@ -0,0 +1,5 @@
+"use strict";
+// Program Written by : Muhammad Shoaib
+// Current Date is :20/02/2024
+//This program will add two digits.
+console.log(5 + 8);
---
+++ Adding_Comments/Program_2.ts
@@ -0,0 +1,4 @@
+// Program Written by : Muhammad Shoaib
+// Current Date is :20/02/2024
+//This program will add two digits.
+console.log(5+8);
