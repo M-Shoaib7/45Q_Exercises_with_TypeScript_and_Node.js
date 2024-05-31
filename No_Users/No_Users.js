6af82dea59ad1b2c0a3bb986ff17dc27e0cad758
M-Shoaib7 <mshoaib249250@gmail.com>
2024-05-31 23:23:43 +0500

    Add files via upload

---
+++ No_Users/No_Users.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = ['admin', 'shoaib', 'abid', 'sajid', 'sabir'];
+if (userName.length === 0) {
+    console.log(' We need to find some users!');
+}
+else {
+    userName = [];
+    console.log('Remove all of the usernames from your array, ' + userName.length);
+}
