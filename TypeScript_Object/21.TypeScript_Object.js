fbd8b38db63ed5ebd25f45050f193138e3a04042
M-Shoaib7 <mshoaib249250@gmail.com>
2024-05-31 23:01:42 +0500

    Add files via upload

---
+++ TypeScript_Object/21.TypeScript_Object.js
@@ -0,0 +1,18 @@
+// object of person , object is like data about data yani k kisi cheez k bary mn information dena .
+var person = {
+    age: 22,
+    name: 'Shoaib',
+    gender: 'Male',
+    nationality: 'Pakistan',
+    student: true
+};
+// yha pr min ny person k object ko print kraya.
+console.log(person);
+// yaha pr main ny ik or object bnaya car ka . ye object bnany ka tarika hi.
+var car = {
+    maker: 'Honda',
+    model: 12,
+    color: 'Black',
+    automatic: false
+};
+console.log(car);
