03749a40147464bd3cb8cac98776af6c871677be
M-Shoaib7 <mshoaib249250@gmail.com>
2024-05-31 22:22:12 +0500

    Add files via upload

---
+++ Name_cases/Name.js
@@ -0,0 +1,17 @@
+"use strict";
+let personName = '';
+personName = prompt('what is your name?') || '';
+let lowercase = personName.toLowerCase();
+let uppercase = personName.toUpperCase();
+let titlecase = personName.split('').map(world => world.charAt(0).toUpperCase() + world.slice(1).
+    toLowerCase()).join('');
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName},Here are your name in:
+    LowerCase: ${lowercase}
+    UpperCase: ${uppercase}
+    TitleCase: ${titlecase}
+    `);
+}
+else {
+    alert('please fill your nme !');
+}
